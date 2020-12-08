67e93fab0a490589ee656cc4228c768bce1ed089
LT-6FGD373\Hannes <h.roever@zertificon.com>
2020-12-08 17:50:22 +0100

    init

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@ public class Main
 {   
    public static void main(String[] args)
    {
-      int participants = 800;
+      int participants = 10;
       int steps = 77;
 
       JosephusGame circle = new JosephusGame();
